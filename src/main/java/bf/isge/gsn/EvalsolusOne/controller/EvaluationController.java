133ab168aa69b7c7648c663a9ae0a0484d2e4db3
TONDE Stanislas Kenneth <73722588+kennsnoww@users.noreply.github.com>
2024-01-06 21:48:14 +0000

    commit
    Signed-off-by: TONDE Stanislas Kenneth <73722588+kennsnoww@users.noreply.github.com>

--- src/main/java/bf/isge/gsn/EvalsolusOne/controller/EvaluationController.java
+++ src/main/java/bf/isge/gsn/EvalsolusOne/controller/EvaluationController.java
@@ -41,7 +41,6 @@ public class EvaluationController {
         return "evaluate";
     }
 
-
     @PostMapping("/evaluate")
     public String submitEvaluation(@RequestParam("criteriaId") Long criteriaId,
                                    @RequestParam("rating") int rating,
@@ -58,55 +57,3 @@ public class EvaluationController {
     }
 
 }
-
-
-/*
-
-package bf.isge.gsn.EvalsolusOne.controller;
-
-import bf.isge.gsn.EvalsolusOne.model.Criteria;
-import bf.isge.gsn.EvalsolusOne.model.Evaluation;
-import bf.isge.gsn.EvalsolusOne.service.EvaluationService;
-import bf.isge.gsn.EvalsolusOne.service.CriteriaService;  // Ajoutez l'import du service CriteriaService
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
-
-@Controller
-public class EvaluationController {
-
-    private final EvaluationService evaluationService;
-    private final CriteriaService criteriaService;  // Ajoutez l'injection du service CriteriaService
-
-    @Autowired
-    public EvaluationController(EvaluationService evaluationService, CriteriaService criteriaService) {
-        this.evaluationService = evaluationService;
-        this.criteriaService = criteriaService;
-    }
-
-    @GetMapping("/evaluationHistory")
-    public String getEvaluationHistory(Model model) {
-        List<Evaluation> evaluationHistory = evaluationService.getAllEvaluations();
-        model.addAttribute("evaluationHistory", evaluationHistory);
-        return "evaluationHistory";
-    }
-
-    @GetMapping("/evaluate")
-    public String showEvaluationForm(Model model) {
-        List<Criteria> criteriaList = criteriaService.getAllCriteria();
-        model.addAttribute("criteriaList", criteriaList);
-        return "evaluate";
-    }
-
-    @PostMapping("/evaluate")
-    public String submitEvaluation(Evaluation evaluation) {
-        evaluationService.submitEvaluation(evaluation);
-        return "redirect:/evaluationHistory";
-    }
-
-}
-*/
